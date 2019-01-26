785b024a947e8912e4023a3ac27423065987f194
Bakhtiar <itsproinc@gmail.com>
2019-01-26 16:23:50 +0700

    Home

---
+++ Home/Assets/Codes/Dialogue.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public string textInput;
+    public float textSpeed;
+    public Image dialogueImage;
+    public Sprite [] dialogueImages;
+
+    public Text textOutput;
+
+    public List<char> letters = new List<char> ();
+
+    public void _DialogueTrigger (string input, int dialogueCharacter)
+    {
+        StartCoroutine (DialogueTrigger (input, 0));
+    }
+
+    IEnumerator DialogueTrigger (string input, int dialogueCharacter)
+    {
+        textInput = input;
+        letters.Clear (); // Reset letters list
+        foreach (char letter in input)
+        {
+            letters.Add (letter);
+        }
+
+        while (textOutput.text != input)
+        {
+            textOutput.text = textOutput.text + letters [0];
+            letters.RemoveAt (0);
+            yield return new WaitForSeconds (textSpeed);
+        }
+    }
+
+    public void SkipText ()
+    {
+        letters.Clear ();
+        textOutput.text = textInput;
+    }
+}
---
+++ Home/Assets/Codes/Home.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Home : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject dialoguePanel;
+
+    public int curHomeTier;
+
+    public int homeTier;
+    public GameObject upgradePanel;
+
+    private void Reset ()
+    {
+        dialoguePanel = GameObject.FindGameObjectWithTag ("UI_Dialogue");
+        dialogue = dialoguePanel.GetComponent<Dialogue> ();
+        upgradePanel = GameObject.FindGameObjectWithTag ("UI_UpgradePanel");
+    }
+
+    public void Start ()
+    {
+        AtHome ();
+    }
+
+    public void AtHome ()
+    {
+        dialoguePanel.SetActive (true);
+        dialogue._DialogueTrigger ("Welcome back home...", 0);
+    }
+}
